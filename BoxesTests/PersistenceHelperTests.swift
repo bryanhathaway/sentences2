17efaa4920233a637959eca2daca1004338aed44
Bryan Hathaway <bryan.hath@gmail.com>
2018-10-17 12:07:13 +1100

    Add PersistenceHelper tests

--- BoxesTests/PersistenceHelperTests.swift
+++ BoxesTests/PersistenceHelperTests.swift
@@ -10,11 +10,44 @@ import XCTest
 @testable import Boxes
 
 class PersistenceHelperTests: XCTestCase {
-    lazy var testablePersistenceHelper = {
-        return PersistenceHelper(fileName: "testableFile.json")
+    lazy var testableHelper = {
+        return PersistenceHelper(fileNameWithExtension: "testableFile.json")
     }()
 
     func testCanRetrieveDefaultFolders() {
-        
+        let helper = PersistenceHelper.default
+
+        let folders = try? helper.read()
+
+        XCTAssert(folders?.first?.title == "Class 1")
+        XCTAssert(folders?.first?.sentences.first?.title == "Alice in Wonderland")
     }
+
+    func testCanSaveAndReadData() {
+        let testPhrase = Phrase(value: "Test Phrase", color: .white)
+        let testSentence = Sentence(title: "Test Sentence",
+                                    compiledSentence: "Test Phrase",
+                                    phrases: [testPhrase],
+                                    color: .blue)
+
+        let testFolder = Folder(title: "Test Folder", sentences: [testSentence], color: .white)
+
+        do {
+            try testableHelper.save(folders: [testFolder])
+
+            let folders = try testableHelper.read()
+            XCTAssert(folders == [testFolder])
+
+        } catch {
+            XCTAssert(false)
+        }
+
+        XCTAssert(true)
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        try? testableHelper.delete()
+    }
+
 }
