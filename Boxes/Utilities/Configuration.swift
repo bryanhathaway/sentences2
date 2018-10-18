64174f9f4c67c4ad99f5093426d1561ac2563e08
Bryan Hathaway <bryan.hath@gmail.com>
2018-10-18 16:45:00 +1100

    Fix default font

--- Boxes/Utilities/Configuration.swift
+++ Boxes/Utilities/Configuration.swift
@@ -12,7 +12,7 @@ import UIKit
 class Configuration {
 
     // MARK: Font
-    var font: UIFont = UIFont.preferredFont(forTextStyle: .body)
+    var font: UIFont = UIFont.defaultFont
 
     func font(ofSize size: CGFloat) -> UIFont {
         return UIFont(name: font.fontName, size: size) ?? .systemFont(ofSize: size)
